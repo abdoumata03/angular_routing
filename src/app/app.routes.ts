54c073e81209df9199217402b38d10ad32344573
MATALLAH <a.matallah@esi-sba.dz>
2024-02-26 11:19:52 +0100

    404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'signup', component: SignupComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', component: NotFoundComponent },
 ];
