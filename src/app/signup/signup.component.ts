2e013751e2a484e4f1da6ffbf6929d6dd3b8e0e7
MATALLAH <a.matallah@esi-sba.dz>
2024-02-28 10:29:36 +0100

    form with html validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, Validators, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ConfirmedValidator } from '../confirm.validator';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-
+  
+  
 }
